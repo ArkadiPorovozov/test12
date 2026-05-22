3692618500c12c6f56a6ef712ed5fdaf1c05c02f
ArkadiPorovozov <arkadiporovoz228@gmail.com>
2026-05-22 22:06:24 +0300

    q0.1

--- prototype/item/recipe.lua
+++ prototype/item/recipe.lua
@@ -2,7 +2,7 @@ local item_sounds = require("__base__.prototypes.item_sounds")
 local item_tints = require("__base__.prototypes.item-tints")
 local space_age_item_sounds = require("__space-age__.prototypes.item_sounds")
 
-
+--[[ -- not use (test//1)
 local q = "__glass-furnace__"
 
 local function make_hr_pipe_layers(main_path, w, h, shift)
@@ -16,17 +16,17 @@ local function make_hr_pipe_layers(main_path, w, h, shift)
         shift = shift or {0, 0},
         scale = 0.5,
       },
---[[
-      {
-        filename = q .. shadow_path,
-        priority = "extra-high",
-        width = 88,
-        height = 64,
-        shift = shadow_shift or shift or {0, 0},
-        scale = 0.5,
-        draw_as_shadow = true,
-      }
-]]
+
+    --  {
+    --    filename = q .. shadow_path,
+    --    priority = "extra-high",
+    --    width = 88,
+    --    height = 64,
+    --    shift = shadow_shift or shift or {0, 0},
+    --    scale = 0.5,
+    --    draw_as_shadow = true,
+    --  }
+
     }
   }
 end
@@ -66,3 +66,4 @@ end
 return {
   glass_furnace_pipe_pictures = glass_furnace_pipe_pictures
 }
+]]
